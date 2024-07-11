fe0d9464df2e8a5d014467b17e4faeeb438f8a70
Joe Meyer <joseph@meyer.id>
2024-07-11 10:57:00 -0500

    move folders to make sure it sees new one

--- src/components/ConfigurationEditor.tsx
+++ src/components/ConfigurationEditor.tsx
@@ -1,6 +1,6 @@
 import { DragDropContext } from "@hello-pangea/dnd";
 import useDragHandlers from "../hooks/useDragEndHandler";
-import { ConfigurationVisualizer } from "./Config/ConfigurationVisualizer";
+import { ConfigurationVisualizer } from "./Configuration/ConfigurationVisualizer";
 
 const ConfigurationEditor = () => {
   const handleDragEnd = useDragHandlers();
